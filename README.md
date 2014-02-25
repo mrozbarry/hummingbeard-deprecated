2b8d1e62a95a4b7c3872ae6580be55303a3f3cf1
Alex Barry <alex.barry@gmail.com>
2014-02-24 23:09:53 -0500

    Some more commenting, because everyone loves inline documentation

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ hummingbeard
 ============
 
 XMPP Framework inspired by [Hummingbird](https://github.com/alagu/hummingbird)
+
+Why make another chat client?
+-----------------------------
+
+Many available chat clients are not extendable, or are only available using a freemium model.  This is not acceptable.  Web developers should have the ability to grab a simple chat widget and just use it, regardless of their protocol.  Chat clients should also be customizable, while using simple markup.  Chat clients should also have easy to follow code.  I believe hummingbeard does all these things, and does them well.
--- js/hummingbeard.js
+++ js/hummingbeard.js
@@ -48,25 +48,35 @@
       // Add a new conversation to the beardbox
       var beardvo = window.document.createElement('div');
       beardvo.className = e.detail.parent.options.chat_dialog_class;
-      beardvo.setAttribute( "data-target_username", e.detail.username );
+      beardvo.setAttribute( "data-chatname", e.detail.chatname );
 
+      // Header/Conversation title
       var heading = window.document.createElement( 'span' );
       heading.className = 'heading';
       heading.innerHTML = e.detail.username;
+
+      // Close button in header
       var close = window.document.createElement( 'a' );
       close.className = 'close';
       close.innerHTML = '&times;';
+      close.onclick = function() {
+        if ( beardvo.style.display == 'block' || !beardvo.style.display ) beardvo.style.display = 'none';
+        else beardvo.style.display = 'block';
+      };
       heading.appendChild( close );
 
       beardvo.appendChild( heading );
 
+      // Container element to put messages into
       var container = window.document.createElement( 'div' );
       container.className = 'container';
       beardvo.appendChild( container );
 
+      // Input box for chatting
       var input = window.document.createElement( 'input' );
       input.className = 'talk';
       input.setAttribute( 'placeholder', 'Type <enter> to send your message' );
+      // Handle pressing enter with text
       input.onkeypress = function( keyevent ) {
         var k = keyevent.keyCode ? keyevent.keyCode : keyevent.which;
         var text = this.value;
@@ -82,17 +92,21 @@
 
       beardvo.appendChild( input );
 
+      // Listen for adding messages to this conversation
       beardvo.addEventListener( "hummingbeard.add_message", function(e){
         var message = window.document.createElement( 'div' );
         message.className = 'message';
+
         var from = window.document.createElement( 'span' );
         from.className = e.detail.parent.options.chat_message_author_class;
         from.innerHTML = e.detail.from + ':&nbsp;';
         message.appendChild( from );
+
         var content = window.document.createTextNode( e.detail.message );
         message.appendChild( content );
         container.appendChild( message );
 
+        // Attempt to scroll or focus the last message
         try {
           container.scrollTop = container.scrollHeight;
         } catch(e) {
@@ -100,38 +114,43 @@
         }
       });
       
+      // Add the conversation to the beardbox (main container)
       e.detail.parent.beardbox.appendChild( beardvo );
     }, false);
     
+    // Get ride of a conversation (user closes the conversation?)
     this.beardbox.addEventListener( "hummingbeard.remove_conversation", function(e){
       // Remove a conversation from the beardbox
     }, false);
     
     window.document.body.appendChild( this.beardbox );
 
-    this.find_conversation = function( user_target ) {
+    // Find an active conversation in the beardbox
+    this.find_conversation = function( chatname ) {
       if ( !user_target ) return null;
       if ( this.beardbox.hasChildNodes() ) {
         var cache = this.beardbox.childNodes;
         for(var c=0; c < cache.length; c++) {
           var beardvo = cache[c];
           // Make sure it is actually a converstation with a target
-          if ( beardvo.hasAttribute( "data-target_username" ) )
+          if ( beardvo.hasAttribute( "data-chatname" ) )
           {
-            if ( beardvo.getAttribute( "data-target_username" ) == user_target ) return beardvo;
+            if ( beardvo.getAttribute( "data-chatname" ) == chatname ) return beardvo;
           }
         }
       }
       return null;
     };
 
-    this.add_conversation = function( user_target ) {
-      var e = new CustomEvent( "hummingbeard.add_conversation", { 'detail': { 'parent': this, 'username': user_target } } );
+    // Add a new conversation to the beardbox
+    this.add_conversation = function( chatname ) {
+      var e = new CustomEvent( "hummingbeard.add_conversation", { 'detail': { 'parent': this, 'chatname': chatname } } );
       this.beardbox.dispatchEvent( e );
     };
 
-    this.add_message = function( user_target, from, message ) {
-      var beardvo = this.find_conversation( user_target );
+    // Add a message to a conversation in the beardbox
+    this.add_message = function( chatname, from, message ) {
+      var beardvo = this.find_conversation( chatname );
       if ( beardvo == null ) return false;
       var e = new CustomEvent( "hummingbeard.add_message", { 'detail': { 'parent': this, 'from': from, 'message': message } } );
       beardvo.dispatchEvent( e );
